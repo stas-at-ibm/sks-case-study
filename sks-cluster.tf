a52faddee1b019259fdaf28d9c4be9c5e3273f1a
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2023-11-13 16:58:16 +0100

    updated cluster config

--- sks-cluster.tf
+++ sks-cluster.tf
@@ -1,15 +1,12 @@
 locals {
-  zone   = "de-fra-1"
+  zone   = "at-vie-1"
   labels = { "project" : "exoscale_sks_case_study_1" }
 }
 
-# This resource will create the control plane
-# Since we're going for the fully managed option, we will ask sks to preinstall
-# the calico network plugin and the exoscale-cloud-controller
 resource "exoscale_sks_cluster" "sj_sks_cluster" {
   zone           = local.zone
   name           = "sj_sks_cluster"
-  version        = "1.22.3"
+  version        = "1.28.3"
   description    = "Case Study 1"
   service_level  = "pro"
   cni            = "calico"
@@ -31,10 +28,10 @@ resource "exoscale_sks_nodepool" "sj_workers" {
 
 resource "exoscale_security_group" "sj_sks_nodes" {
   name        = "sks_nodes"
-  description = "Allows traffic between sks nodes and public pulling of logs"
+  description = "Allows traffic between sks nodes, public pulling of logs and open node ports"
 }
 
-resource "exoscale_security_group_rule" "sks_nodes_logs_rule" {
+resource "exoscale_security_group_rule" "sj_sks_nodes_logs_rule" {
   security_group_id = exoscale_security_group.sj_sks_nodes.id
   type              = "INGRESS"
   protocol          = "TCP"
